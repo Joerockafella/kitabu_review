f5e6ae21c663caf842d42cf8ee70686cb888ce4f
Joerockafella <joeshindano@outlook.com>
2019-07-13 13:30:12 +0200

    kitabu 1.0.0 (New updates: Login, Logout, users can have a profile and are able update it as well as upload a profile image )

--- kitabu_review/settings.py
+++ kitabu_review/settings.py
@@ -129,4 +129,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'main-home'
+LOGIN_URL = 'login'
