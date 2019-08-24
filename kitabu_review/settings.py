86f32091c1010b1e86cb5965912132bdf8a8312c
Joerockafella <joeshindano@outlook.com>
2019-08-24 19:08:34 +0200

    kitabu 2.3.0 (settings upaded)

--- kitabu_review/settings.py
+++ kitabu_review/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "ya3u=3w@ac_bfhbho-8eohj2413=(gk%=6%qj#vrp_s1z4u%9y"
+# SECRET_KEY = "ya3u=3w@ac_bfhbho-8eohj2413=(gk%=6%qj#vrp_s1z4u%9y"
+SECRET_KEY = os.environ.get("KITABU_REVIEW_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get("KITABU_DEBUG_VALUE") == "True"
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["kitabureview.herokuapp.com"]
 
 
 # Application definition
